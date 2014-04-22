890d2a3c444537e0ad3265cd43cff1d27ecbb620
Zygmunt Łenyk <zygmuntlenyk@gmail.com>
2014-04-22 19:45:14 +0200

    Clean test

--- src/gui/Test02.java
+++
@@ -1,9 +0,0 @@
-package gui;
-
-public class Test02 {
-	public static void main(String[] args){
-		String a = "a";
-		
-		System.out.print(a);
-	}
-}
--- src/mod/Config.java
+++ src/mod/Config.java
@@ -2,43 +2,10 @@ package mod;
 
 /**
  * @author zygmunt
- *	Stores main configuration for the game and visual appearance.
+ *	Stores main configuration for the game.
  */
 public class Config {
-	private static int HEIGHT;
-	private static int WIDTH;
 	private static int PLAYERS;
-	/**
-	 * @return height of the main window.
-	 */
-	public static int getHEIGHT() {
-		return HEIGHT;
-	}
-	/**
-	 * @param hEIGHT
-	 * @throws InvalidConfigException
-	 * Sets height of the main window.
-	 */
-	public static void setHEIGHT(int hEIGHT) throws InvalidConfigException {
-		if(hEIGHT < 200) throw new InvalidConfigException("Height below 200.");
-		HEIGHT = hEIGHT;
-	}
-	/**
-	 * @return width of the main win
-	 * @param hEIGHTdow.
-	 */
-	public static int getWIDTH() {
-		return WIDTH;
-	}
-	/**
-	 * @param wIDTH
-	 * @throws InvalidConfigException
-	 * Sets width of the main window.
-	 */
-	public static void setWIDTH(int wIDTH) throws InvalidConfigException {
-		if(wIDTH < 200) throw new InvalidConfigException("Width below 200.");
-		WIDTH = wIDTH;
-	}
 	/**
 	 * @return number of players.
 	 */
@@ -48,8 +15,10 @@ public class Config {
 	/**
 	 * @param pLAYERS
 	 * Sets number of players.
+	 * @throws InvalidConfigException 
 	 */
-	public static void setPLAYERS(int pLAYERS) {
+	public static void setPLAYERS(int pLAYERS) throws InvalidConfigException {
+		if(pLAYERS < 2 || pLAYERS > 4) throw new InvalidConfigException("Wrong number of players.");
 		PLAYERS = pLAYERS;
 	}
 
