02a6c91c02a944e0f39d417906743c8e81de1539
Zygmunt Łenyk <zygmuntlenyk@gmail.com>
2014-04-28 21:37:49 +0200

    Basic Menu layout.

--- src/gui/App.java
+++ src/gui/App.java
@@ -3,10 +3,17 @@ package gui;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import java.awt.GridLayout;
+import javax.swing.JButton;
+import java.awt.GridBagLayout;
+import javax.swing.SpringLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.BoxLayout;
 
 public class App {
 
-	private JFrame frame;
+	private JFrame frmNeuroshima;
 
 	/**
 	 * Launch the application.
@@ -16,7 +23,7 @@ public class App {
 			public void run() {
 				try {
 					App window = new App();
-					window.frame.setVisible(true);
+					window.frmNeuroshima.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -35,9 +42,26 @@ public class App {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	}
+		frmNeuroshima = new JFrame();
+		frmNeuroshima.setTitle("Neuroshima");
+		frmNeuroshima.setResizable(false);
+		frmNeuroshima.setBounds(100, 100, 233, 300);
+		frmNeuroshima.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmNeuroshima.getContentPane().setLayout(null);
+		
+		JButton newGameButton = new JButton("New Game");
+		newGameButton.setBounds(56, 30, 117, 25);
+		frmNeuroshima.getContentPane().add(newGameButton);
+		
+		JButton somethingElseButton = new JButton("Something Else");
+		somethingElseButton.setBounds(56, 80, 117, 25);
 
+
+		frmNeuroshima.getContentPane().add(somethingElseButton);
+		
+		JButton networkGameButton = new JButton("Network Game");
+		networkGameButton.setEnabled(false);
+		networkGameButton.setBounds(56, 129, 117, 25);
+		frmNeuroshima.getContentPane().add(networkGameButton);
+	}
 }
