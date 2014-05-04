b2df468d459d598a7aed05a27bb4d2a5c0278264
Radian94 <bloodrian@gmail.com>
2014-05-04 20:45:33 +0200

    Close button works

--- src/gui/StartMenu.java
+++ src/gui/StartMenu.java
@@ -49,19 +49,26 @@ public class StartMenu {
 		frmNeuroshima = new JFrame();
 		frmNeuroshima.setTitle("Neuroshima");
 		frmNeuroshima.setResizable(false);
-		frmNeuroshima.setBounds(100, 100, 233, 300);
+		frmNeuroshima.setBounds(100, 100, 275, 300);
 		frmNeuroshima.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmNeuroshima.getContentPane().setLayout(null);
 
 		JButton somethingElseButton = new JButton("Something Else");
 		JButton networkGameButton = new JButton("Network Game");
+		
 		JButton btnNewButton = new JButton("Close");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				System.exit(0);
+			}
+		});
+		
 		JButton newGameButton = new JButton("New Game");
 		
 		newGameButton.setBounds(56, 30, 117, 25);
 		somethingElseButton.setBounds(56, 80, 117, 25);
-		networkGameButton.setBounds(56, 129, 117, 25);
-		btnNewButton.setBounds(56, 175, 117, 25);
+		networkGameButton.setBounds(56, 130, 117, 25);
+		btnNewButton.setBounds(56, 180, 117, 25);
 
 		networkGameButton.setEnabled(false);
 		
@@ -92,7 +99,7 @@ public class StartMenu {
 	private JList<String> createList(){
 
 		JList<String> list = new JList<String>(Config.getArmies());
-		list.setBounds(185, 34, 34, 78);
+		list.setBounds(185, 34, 74, 78);
 		
 		return list;
 	}
