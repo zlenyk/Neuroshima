b24b9a749f05fd80cd1e4ac51330d9232b34a34d
KYUUBl <Kamilch6@gmail.com>
2014-05-16 19:28:54 +0200

    Sztaby

---
+++ src/mod/tiles/Hegemonia/SztabHegemonia.java
@@ -0,0 +1,50 @@
+package mod.tiles.Hegemonia;
+
+import gui.Field;
+
+import java.awt.Image;
+import java.util.LinkedList;
+
+import cont.board.ImageLoader;
+
+public class SztabHegemonia extends mod.Unit {
+	
+	public SztabHegemonia(){
+		field = new Field(getImage(),getSelectedImage());
+
+		position = -1;
+		//this.rotation = rotation;
+		hp=1;
+		shield = new boolean[6];
+		for(int i=0;i<6;i++) shield[i] = false;
+		isNetted = 0;
+		initiative = new LinkedList<Integer>();
+		initiative.add(0);
+		shootBonus = 0;
+		hitBonus = 0;
+		initiativeBonus = 0;
+	}
+	
+	
+	public void attack(){
+		hit(0,1+hitBonus);
+		hit(1,1+hitBonus);
+		hit(2,1+hitBonus);
+		hit(3,1+hitBonus);
+		hit(4,1+hitBonus);
+		hit(5,1+hitBonus);
+	}
+
+	@Override
+	public Image getImage() {
+		
+		return ImageLoader.Hegemonia.sztab();
+	}
+
+	@Override
+	public Image getSelectedImage() {
+		
+		return ImageLoader.Hegemonia.sztabSelected();
+	}
+}
+
